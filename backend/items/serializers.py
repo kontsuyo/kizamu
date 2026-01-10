37e40b0271f1fb09878caaff1fbfc817c06ccf39
Chang Doe <kontsuyo@users.noreply.github.com>
2026-01-10 10:37:49 +0900

    ユーザとブーツの紐づけと権限を追加

--- backend/items/serializers.py
+++ backend/items/serializers.py
@@ -4,6 +4,8 @@ from items.models import BootItem
 
 
 class BootItemSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source="user.username")
+
     class Meta:
         model = BootItem
         fields = ["id", "user", "brand", "model", "leather", "created_at"]
--- backend/items/views.py
+++ backend/items/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from items.models import BootItem
 from items.serializers import BootItemSerializer
@@ -7,8 +7,13 @@ from items.serializers import BootItemSerializer
 class BootItemList(generics.ListCreateAPIView):
     queryset = BootItem.objects.all()
     serializer_class = BootItemSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class BootItemDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = BootItem.objects.all()
     serializer_class = BootItemSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
