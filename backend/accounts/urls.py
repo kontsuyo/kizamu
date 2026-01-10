3992e22b6e37b66f9816f929940e9c0ac18c3364
Chang Doe <kontsuyo@users.noreply.github.com>
2026-01-10 21:23:10 +0900

    fix: 冗長だったため

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -4,8 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from accounts import views
 
 urlpatterns = [
-    path("users/", views.CustomUserList.as_view(), name="customuser-list"),
-    path("users/<int:pk>/", views.CustomUserDetail.as_view(), name="customuser-detail"),
+    path("users/", views.CustomUserList.as_view(), name="user-list"),
+    path("users/<int:pk>/", views.CustomUserDetail.as_view(), name="user-detail"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/items/urls.py
+++ backend/items/urls.py
@@ -4,8 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from items import views
 
 urlpatterns = [
-    path("boots/", views.BootItemList.as_view(), name="bootitem-list"),
-    path("boots/<int:pk>/", views.BootItemDetail.as_view(), name="bootitem-detail"),
+    path("boots/", views.BootItemList.as_view(), name="boot-list"),
+    path("boots/<int:pk>/", views.BootItemDetail.as_view(), name="boot-detail"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
