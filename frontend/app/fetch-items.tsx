96d1b74a8b9038eb6532ebb3da8d83039ea2daf2
Chang Doe <kontsuyo@users.noreply.github.com>
2026-01-26 21:18:52 +0900

    [remove]中身を呼び出し元に移動

--- frontend/app/fetch-items.tsx
+++
@@ -1,8 +0,0 @@
-import { Item } from "./types";
-
-export async function fetchItems(): Promise<Item[]> {
-  const url = "http://backend:8000/";
-  const res = await fetch(url);
-  const data = await res.json();
-  return data.results;
-}
