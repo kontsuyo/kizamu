8352b04d26902460e0de3ca1a9ef8dbeb7c35cab
Chang Doe <kontsuyo@users.noreply.github.com>
2026-01-26 21:20:36 +0900

    [clean]関数を移動し、エラーハンドリングを追加

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,16 +1,36 @@
-import { fetchItems } from "./fetch-items";
-import {Item} from "./types"
+import { Item, PaginatedResponse } from "./types";
 
-export default async function Feed() {
+async function fetchItems(): Promise<Item[]> {
+  const url = "http://backend:8000/";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`レスポンスステータス: ${response.status}`);
+    }
+    const json: PaginatedResponse<Item> = await response.json();
+    return json.results;
+  } catch (error) {
+    console.error("データ取得エラー:", error);
+    return [];
+  }
+}
+
+export default async function Home() {
   const results = await fetchItems();
-  const listItems = results.map((item: Item) =>
+  const listItems = results.map((item: Item) => (
     <li key={item.id}>
-      <div>{item.brand} {item.model_name}</div>
+      <div>
+        {item.brand} {item.model_name}
+      </div>
       <div>{item.leather}</div>
-      <div><img src={item.image} alt="" /></div>
-      <div>{item.user} {item.created_at}</div>
+      <div>
+        <img src={item.image} alt="" />
+      </div>
+      <div>
+        {item.user} {item.created_at}
+      </div>
     </li>
-  )
+  ));
   return (
     <div>
       <ul>{listItems}</ul>
