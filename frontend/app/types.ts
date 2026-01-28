532576744359abddfa5e0a4bf424a7509c79490c
Chang Doe <kontsuyo@users.noreply.github.com>
2026-01-29 06:04:38 +0900

    [change]ファイル名を変更

---
+++ frontend/app/types.ts
@@ -0,0 +1,61 @@
+export interface Post {
+  id: number;
+  item_id: number;
+  brand: string;
+  model_name: string;
+  leather: string;
+  image: string;
+  note: string;
+  user: string;
+  created_at: string;
+}
+
+export interface PaginatedResponse<T> {
+  next: string | null;
+  previous: string | null;
+  results: T[];
+}
+
+export interface Item {
+  id: number;
+  brand: string;
+  model_name: string;
+  leather: string;
+  user: string;
+  created_at: string;
+  photos: [];
+}
+
+export interface ItemPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export interface Photo {
+  id: number;
+  item: number;
+  image: string;
+  wore_on: string;
+  note: string;
+  shared_feed: boolean;
+  user: string;
+}
+
+export interface PhotoPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export interface UserProfile {
+  id: number;
+  username: string;
+  items: [Item];
+}
+
+export interface ProfilePageProps {
+  params: {
+    username: string;
+  };
+}
