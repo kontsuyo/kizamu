426773a054b6fc5e947876472da28c56450d85ce
Chang Doe <kontsuyo@users.noreply.github.com>
2026-01-30 07:32:40 +0900

    [add]ホームリンク追加

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,18 @@
+import Link from "next/link";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const Header = () => {
+    return (
+      <div>
+        <Link href={`/`}>
+          <h1>Aging Gallery</h1>
+        </Link>
+      </div>
+    );
+  };
   return (
     <html>
+      <Header />
       <body>{children}</body>
     </html>
   );
