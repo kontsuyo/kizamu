6deb59c554886df25b0ef56df6510e4a314e1d0c
Chang Doe <kontsuyo@users.noreply.github.com>
2026-02-01 11:57:30 +0900

    [add]Railwayでデプロイするのに必要なファイルや設定を追加

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -74,6 +74,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -152,6 +153,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "media/"  # メディアファイルのURL
 MEDIA_ROOT = BASE_DIR / "media"  # 画像などのメディアファイルの保存先
 
