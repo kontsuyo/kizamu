0597be0dfbf1551ece24807cd6327014e3328317
Chang Doe <kontsuyo@users.noreply.github.com>
2026-02-03 19:52:41 +0900

    [add]CORS設定

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -1,7 +1,8 @@
-from .base import *
+import os
 
 import dj_database_url
-import os
+
+from .base import *
 
 DEBUG = False
 ALLOWED_HOSTS = ["*"]
@@ -10,6 +11,20 @@ DATABASES = {
         default=os.environ.get("DATABASE_URL"), conn_max_age=600
     )
 }
+
+# Vercelのドメインを許可リストに追加
+CORS_ALLOWED_ORIGINS = [
+    "https://aging-gallary.vercel.app",  # Vercelで発行されたURL
+]
+
+# DjangoのCSRF対策でもVercelを信用する
+CSRF_TRUSTED_ORIGINS = [
+    "https://aging-gallary.vercel.app",
+]
+
+# cookieなど使う場合はTrue
+CORS_ALLOW_CREDENTIALS = True
+
 # 本番DBやセキュリティ設定
 # SECURE_SSL_REDIRECT = True
 # SESSION_COOKIE_SECURE = True
